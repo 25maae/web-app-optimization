994042af6439d23369388e1e32f64d3f1e5dfe11
25maae <eaa25maae@students.eaaa.dk>
2026-08-21 10:46:23 +0200

    Add Greeting and Welcome components; update HomePage to use Greeting

--- src/sandbox/sandbox.js
+++ src/sandbox/sandbox.js
@@ -1,3 +1,18 @@
 import { teachers } from "./teachers";
 
 console.log(teachers);
+
+function showWelcomeMessage() {
+    console.log("Welcome to the Sandbox!");
+}
+
+showWelcomeMessage();
+
+function sayHi(name) {
+    console.log(`Hi, ${name}!`);
+}
+sayHi("Alice");
+sayHi("Bob");
+sayHi("Charlie");
+
+export { sayHi };
